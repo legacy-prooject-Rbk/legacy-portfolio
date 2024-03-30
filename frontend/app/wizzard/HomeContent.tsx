764b26c5e33b25cfbf2457884758cc8f5c0ba553
selim021 <brahamselim09@gmail.com>
2024-03-30 15:47:21 +0100

    added css

--- frontend/app/wizzard/HomeContent.tsx
+++ frontend/app/wizzard/HomeContent.tsx
@@ -1,4 +1,5 @@
-"use client";
+
+"use client"
 
 import { useState, useEffect, ChangeEvent } from 'react';
 import axios from 'axios';
@@ -22,34 +23,24 @@ const HomeContent: NextPage = () => {
   const [suggestions, setSuggestions] = useState<Portfolio[]>([]);
 
   useEffect(() => {
-    const fetchAllCards = async () => {
-      try {
-        const response = await axios.get<Portfolio[]>('http://localhost:3000/api/portfolio');
-        setPortfolio(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    
     fetchAllCards();
   }, []);
+
+  const fetchAllCards = async () => {
+    try {
+      const response = await axios.get<Portfolio[]>('http://localhost:3000/api/portfolio');
+      setPortfolio(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const onSuggestHandler = (fullName: string) => {
     setQuery(fullName);
     setSuggestions([]);
   };
 
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const newQuery = e.target.value;
-    setQuery(newQuery);
-    if (newQuery.length > 0) {
-      const regex = new RegExp(`${newQuery}`, "gi");
-      const matches = portfolio.filter(user => user.fullName.match(regex));
-      setSuggestions(matches);
-    } else {
-      setSuggestions([]);
-    }
-  };
+
 
   const handleSearch = async () => {
     try {
@@ -71,7 +62,72 @@ const HomeContent: NextPage = () => {
   ];
 
   return (
-    <div></div>
+    <div className="flex flex-col min-h-screen bg-white">
+      <header className="bg-transparent shadow-md py-4">
+        <div className="container mx-auto flex justify-center items-center">
+          <Image src="/images/pro.png" alt="ProPlex Logo" width={64} height={64} />
+        </div>
+      </header>
+
+      <div className="flex justify-center items-center h-[500px]">
+        <div className="p-6 bg-[#0101018a] rounded-lg">
+          <h1 className="text-6xl font-bold mb-6">Discover Professionals near you</h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 relative gap-4 items-center">
+            <input
+              type="text"
+              value={query}
+              placeholder="What are you looking for?"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md"
+             
+            />
+            {suggestions.map((suggestion, i) => (
+              <div
+                className="absolute w-full top-full mt-1 rounded shadow bg-white"
+                key={i}
+            
+              >
+                {suggestion.fullName}
+              </div>
+            ))}
+
+            <select
+              className="w-full px-4 py-2 border border-gray-300 rounded-md bg-white text-black"
+              value={selectedCity}
+              onChange={(e) => setSelectedCity(e.target.value)}
+            >
+              <option value="">...</option>
+              {cities.map((city, index) => (
+                <option key={index} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+            <button
+              className="px-6 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-700"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto py-16">
+        <h2 className="text-3xl font-bold text-center text-black mb-10">DISCOVER Trends</h2>
+        <div className="flex flex-wrap justify-center gap-4">
+          {cards.map((trend) => (
+            <div key={trend.id} className="bg-white rounded-lg shadow-lg p-4 text-center mb-4">
+              <Image src={trend.photo} alt={trend.fullName} width={80} height={80} />
+              <h3 className="font-bold text-gray-800">{trend.fullName}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      
+      </div>
+    
   );
 };
 
